c96b085ead816d3fe602ada63f9c920a819cfc3f
reallyshow <50952995+sharakovia@users.noreply.github.com>
2021-07-15 21:01:12 +0800

    AndroidProducts.mk: Bringup exthm

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -15,9 +15,9 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/lineage_maple_dsds.mk
+    $(LOCAL_DIR)/exthm_maple_dsds.mk
 
 COMMON_LUNCH_CHOICES := \
-    lineage_maple_dsds-user \
-    lineage_maple_dsds-userdebug \
-    lineage_maple_dsds-eng
+    exthm_maple_dsds-user \
+    exthm_maple_dsds-userdebug \
+    exthm_maple_dsds-eng
